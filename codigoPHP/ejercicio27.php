6e80b89ec3f569e2546b9b5a21a1102e733b5e0a
Gonzalo Junquera Lorenzo <gonzalo.junlor@educa.jcyl.es>
2025-10-26 23:23:53 +0100

    ej27 logica php y diseño

--- codigoPHP/ejercicio27.php
+++ codigoPHP/ejercicio27.php
@@ -6,25 +6,31 @@
     <link rel="stylesheet" href="../webroot/css/estilos.css">
     <title>Gonzalo Junquera Lorenzo</title>
     <style>
-        .container {
-            
-            max-width: 800px;
+        @font-face {
+            font-family: LibreCaslon;
+            src: url(../webroot/fonts/Libre_Caslon_Text/LibreCaslonText-Italic.ttf);
+        }
+        .contenedor {
+            max-width: 900px;
             margin: 0 auto;
             padding: 20px;
             background-color: #fff;
             border: 1px solid #ccc;
         }
-
+        .contenedor *{
+            font-family: LibreCaslon;
+            letter-spacing: 1px;
+        }
         /* Títulos */
-        h1 {
-            color: #4CAF50;
+        .contenedor h1 {
+            color: #12762c;
             text-align: center;
             font-size: 1.5em;
             margin-bottom: 5px;
         }
 
-        h2 {
-            color: #4CAF50;
+        .contenedor h2 {
+            color: #12762c;
             text-align: center;
             font-size: 1.5em;
             margin-bottom: 20px;
@@ -74,7 +80,7 @@
 
         /* Estilos específicos para campo nombre_apellidos */
         #nombre_apellidos {
-            width: 80%;
+            width: 50%;
             max-width: 500px; 
         }
 
@@ -122,7 +128,7 @@
         }
 
         /* Botón de ENVIAR */
-        .boton-container {
+        .boton-contenedor {
             text-align: center;
             margin-top: 30px;
         }
@@ -134,6 +140,18 @@
             color: #195b13ff;
             font-size: 1em;
             cursor: pointer;
+        }
+
+        /* Respuesta */
+        .respuesta-contenedor{
+            padding-left: 100px;
+            word-spacing: 5px;
+        }
+        .respuesta-contenedor p{
+            margin-bottom: 10px;
+        }
+        .respuesta{
+            color: #5983b0;
         }
     </style>
 </head>
@@ -175,9 +193,9 @@
 
             // Validamos los datos del formulario
             $aErrores['nombre_apellidos']= validacionFormularios::comprobarAlfabetico($_REQUEST['nombre_apellidos'],100,0,1,);
-            $aErrores['fecha_nacimiento']= validacionFormularios::validarFecha($_REQUEST['fecha_nacimiento']);
+            $aErrores['fecha_nacimiento']= validacionFormularios::validarFecha($_REQUEST['fecha_nacimiento'],'01/01/2200','01/01/1900',1);
             // $aErrores['sentimiento'] = $_REQUEST['sentimiento'];
-            $aErrores['puntuacion_curso']= validacionFormularios::comprobarEntero($_REQUEST['puntuacion_curso']);
+            $aErrores['puntuacion_curso']= validacionFormularios::comprobarEntero($_REQUEST['puntuacion_curso'],PHP_INT_MAX,-PHP_INT_MAX,1);
             $aErrores['vacaciones'] = validacionFormularios::comprobarNoVacio($_REQUEST['vacaciones']);
             $aErrores['estado_animo'] = validacionFormularios::comprobarNoVacio($_REQUEST['estado_animo']);
 
@@ -203,77 +221,83 @@
             $aRespuestas['vacaciones'] = $_REQUEST['vacaciones'];
             $aRespuestas['estado_animo'] = $_REQUEST['estado_animo'];
             
+            // Calculo de la edad restando a la fecha actual la fecha de nacimiento
+            $ofechaActual = new DateTime();
+            $oFechaNacimento = new DateTime($aRespuestas['fecha_nacimiento']);
+            $oDiferenciaFechas = $ofechaActual->diff($oFechaNacimento);
+
             // Repuesta con texto largo
-            // RESULTADOS DE LA ENCUESTA – INFORME DE SATISFACCIÓN PERSONAL
-            // Imagen
-            /* Hoy (fechaActualConDiaDeLaSemanaYNombreMes) a las (hora actual HH:MM) .
-D. (nombreYApellidos) nacido hace (añosDeEdad) años se siente (comoSeSiente).
-Valora su curso actual con (valoración) sobre 10.
-Estas navidades las dedicará a (dedicaciónNavideña).
-Y, además, opina que:
-(Opinión de varias lineas) */
-            echo "<h2>Resultados:</h2>";
-            foreach ($aRespuestas as $campo => $valor) {
-                echo "<p>$campo: <b>$valor</b></p>";
-            }
+            echo '<div class="contenedor">';
+            echo '<h2>RESULTADOS DE LA ENCUESTA - INFORME DE SATISFACCIÓN PERSONAL</h2>';
+            echo '<div class="logo"><img src="../webroot/media/images/resultado.png" alt="logo"></div>';
+            echo '<div class="respuesta-contenedor">';
+            echo "<p>Hoy <span class='respuesta'>".$ofechaActual->format("l, d F Y")."</span>  a las <span class='respuesta'>".$ofechaActual->format("H:i")."</span>  .</p>";
+            echo "<p>D. <span class='respuesta'>".$aRespuestas['nombre_apellidos']."</span> nacido hace <span class='respuesta'>".$oDiferenciaFechas->y."</span>  años se siente <span class='respuesta'>".$aRespuestas['sentimiento']."</span> .</p>";
+            echo "<p>Valora su curso actual con <span class='respuesta'>".$aRespuestas['puntuacion_curso']."</span>  sobre 10.</p>";
+            echo "<p>Estas navidades las dedicará a <span class='respuesta'>".$aRespuestas['vacaciones']."</span> .</p>";
+            echo "<p>Y, además, opina que:</p>";
+            echo "<p><pre><span class='respuesta'>".$aRespuestas['estado_animo']."</span></pre> </p>";
+            echo '</div>';
+            echo '</div>';
             
         } else { //Mostrar el formulario hasta que lo rellenemos correctamente
             //Mostrar formulario
             //Mostrar los datos tecleados correctamente en intentos anteriores
             //Mostrar mensajes de error (si los hay y el formulario no se muestra por primera vez)
             ?>
-                <div class="container">
-                    <h1>ENCUESTA INDIVIDUAL DE VALORACIÓN – EJERCICIO 27</h1>
+                <div class="contenedor">
+                    <h1>ENCUESTA INDIVIDUAL DE VALORACIÓN - EJERCICIO 27</h1>
                     <h2>UTILIZANDO PLANTILLA DE DESARROLLO DE FORMULARIOS COMO CHURROS</h2>
                     
                     <div class="logo">
-                        <img src="../webroot/media/images/ies2.png" alt="logo">
+                        <img src="../webroot/media/images/encuesta.png" alt="logo">
                     </div>
 
                     <form action="#" method="POST" class="formulario">
                         <div class="campo">
-                            <label for="nombre_apellidos">Nombre y apellidos del alumno: <span class="obligatorio">(*)</span></label>
-                            <input type="text" id="nombre_apellidos" name="nombre_apellidos">
+                            <label for="nombre_apellidos">Nombre y apellidos del alumno: <span class="obligatorio">(*<?php echo $aErrores['nombre_apellidos']??'' ?>)</span></label>
+                            <input type="text" id="nombre_apellidos" name="nombre_apellidos" value="<?php echo $_REQUEST['nombre_apellidos']??'' ?>">
                         </div>
 
                         <div class="campo">
-                            <label for="fecha_nacimiento">Fecha de nacimiento: <span class="obligatorio">(*)</span></label>
-                            <input type="date" id="fecha_nacimiento" name="fecha_nacimiento">
+                            <label for="fecha_nacimiento">Fecha de nacimiento: <span class="obligatorio">(*<?php echo $aErrores['fecha_nacimiento']??'' ?>)</span></label>
+                            <input type="date" id="fecha_nacimiento" name="fecha_nacimiento" value="<?php echo $_REQUEST['fecha_nacimiento']??'' ?>">
                         </div>
 
                         <div class="campo">
                             <label>¿Cómo te sientes hoy? <span class="obligatorio">(* y una única selección permitida)</span> </label>
                             <div class="opciones-radio">
-                                <label>MUY MAL</label><input type="radio" name="sentimiento" value="MUY MAL">
-                                <label>MAL</label><input type="radio" name="sentimiento" value="MAL">
-                                <label>REGULAR</label><input type="radio" name="sentimiento" value="REGULAR">
-                                <label>BIEN</label><input type="radio" name="sentimiento" value="BIEN">
-                                <label>MUY BIEN</label><input type="radio" name="sentimiento" value="MUY BIEN">
+                                <label>MUY MAL</label><input type="radio" name="sentimiento" value="MUY MAL" <?php echo $_REQUEST['sentimiento']=="MUY MAL"?'checked':'' ?>>
+                                <label>MAL</label><input type="radio" name="sentimiento" value="MAL" <?php echo $_REQUEST['sentimiento']=="MAL"?'checked':'' ?>>
+                                <label>REGULAR</label><input type="radio" name="sentimiento" value="REGULAR" <?php echo $_REQUEST['sentimiento']=="REGULAR"?'checked':'' ?>>
+                                <label>BIEN</label><input type="radio" name="sentimiento" value="BIEN" <?php echo $_REQUEST['sentimiento']=="BIEN"?'checked':'' ?>>
+                                <label>MUY BIEN</label><input type="radio" name="sentimiento" value="MUY BIEN" <?php echo $_REQUEST['sentimiento']=="MUY BIEN"?'checked':'' ?>>
                             </div>
                         </div>
 
                         <div class="campo">
-                            <label for="puntuacion_curso">¿Cómo va el curso? [0-10]: <span class="obligatorio">(* y entero)</span></label>
-                            <input type="number" id="puntuacion_curso" name="puntuacion_curso" min="0" max="10">
+                            <label for="puntuacion_curso">¿Cómo va el curso? [0-10]: <span class="obligatorio">(* y entero)<?php echo $aErrores['puntuacion_curso']??'' ?></span></label>
+                            <input type="number" id="puntuacion_curso" name="puntuacion_curso" min="0" max="10" value="<?php echo $_REQUEST['puntuacion_curso']??'' ?>">
                         </div>
 
                         <div class="campo">
-                            <label for="vacaciones">¿Cómo se presentan las vacaciones de navidad? <span class="obligatorio">(* Cuadro de lista con los siguientes valores posibles: Ni idea, Con la familia, De fiesta, Trabajando, Estudiando DWES)</span></label>
+                            <label for="vacaciones">¿Cómo se presentan las vacaciones de navidad? <span class="obligatorio">(* Cuadro de lista con los siguientes valores posibles: Ni idea, Con la familia, De fiesta, Trabajando, Estudiando DWES)<?php echo $aErrores['vacaciones']??'' ?></span></label>
                             <select id="vacaciones" name="vacaciones">
-                                <option value="" disabled selected hidden></option> <option value="Ni idea">Ni idea</option>
-                                <option value="Con la familia">Con la familia</option>
-                                <option value="De fiesta">De fiesta</option>
-                                <option value="Trabajando">Trabajando</option>
-                                <option value="Estudiando DWES">Estudiando DWES</option>
+                                <option value="" disabled selected hidden></option> 
+                                <option value="Ni idea" <?php echo $_REQUEST['vacaciones']=="Ni idea"?'selected':'' ?>>Ni idea</option>
+                                <option value="Con la familia" <?php echo $_REQUEST['vacaciones']=="Con la familia"?'selected':'' ?>>Con la familia</option>
+                                <option value="De fiesta" <?php echo $_REQUEST['vacaciones']=="De fiesta"?'selected':'' ?>>De fiesta</option>
+                                <option value="Trabajando" <?php echo $_REQUEST['vacaciones']=="Trabajando"?'selected':'' ?>>Trabajando</option>
+                                <option value="Estudiando DWES" <?php echo $_REQUEST['vacaciones']=="Estudiando DWES"?'selected':'' ?>>Estudiando DWES</option>
                             </select>
                         </div>
 
                         <div class="campo">
-                            <label for="estado_animo">Describe brevemente tu estado de ánimo: <span class="obligatorio">(* y área de texto para varias líneas)</span></label>
-                            <textarea id="estado_animo" name="estado_animo" rows="3"></textarea>
+                            <label for="estado_animo">Describe brevemente tu estado de ánimo: <span class="obligatorio">(* y área de texto para varias líneas)<?php echo $aErrores['estado_animo']??'' ?></span></label>
+                            <textarea id="estado_animo" name="estado_animo" rows="3"><?php echo $_REQUEST['estado_animo']??'' ?></textarea>
                         </div>
 
-                        <div class="boton-container">
+                        <div class="boton-contenedor">
                             <button type="submit" class="enviar" name="enviar">ENVIAR</button>
                         </div>
                     </form>
